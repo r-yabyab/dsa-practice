3d7dfcdd0e75dfca6e45f4b95a7cb6502bceb322
Rod C <cayabyabrr@yahoo.com>
2026-03-30 17:41:05 -0700

    comps

---
+++ src/comprehensions.py
@@ -0,0 +1,20 @@
+def main():
+    pass
+
+def list_comprehension():
+    list_comp = [x for x in range(1, 5)]
+    print(list_comp)
+
+def dict_comprehension():
+    data = {
+        "a": 5,
+        "b": 12,
+        "c": 20
+    }
+    print(data.items())
+    
+    data_filtered = {k : v for k,v in data.items() if v>10}
+    print(data_filtered)
+
+
+dict_comprehension()
